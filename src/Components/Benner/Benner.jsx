b57dc5663629dcb0ce437bb271633f92c17b490c
Mahammud Hassan Limon <156072234+Limon-Hassan@users.noreply.github.com>
2025-03-22 03:13:47 +0600

    done

--- src/Components/Benner/Benner.jsx
+++ src/Components/Benner/Benner.jsx
@@ -7,10 +7,10 @@ const Benner = () => {
     <>
       <section>
         <Container>
-          <div className="bg-[#000000] flex gap-[38px] mt-[40px]">
+          {/* <div className="bg-[#000000] flex gap-[38px] mt-[40px]">
             <div className="pt-[58px] ml-[64px]">
               <div className="flex items-center gap-[24px] mb-[20px]">
-                <i class="fa-brands fa-apple text-[40px] text-[#FFF]"></i>
+                <i className="fa-brands fa-apple text-[40px] text-[#FFF]"></i>
                 <span className="text-[16px] font-Poppipns_FONT font-medium leading-6 text-[#FFF]">
                   iPhone 14 Series
                 </span>
@@ -21,16 +21,225 @@ const Benner = () => {
               <button className=" mb-[48px]">
                 <a
                   href="#"
-                  class="relative text-white text-lg font-medium inline-flex items-center gap-2 after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-[81px] hover:after:w-full after:ease-in-out after:h-[1.5px] after:bg-white after:scale-x-100 after:transition-transform after:duration-300 group"
+                  className="relative text-white text-lg font-medium inline-flex items-center gap-2 after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-[81px] hover:after:w-full after:ease-in-out after:h-[1.5px] after:bg-white after:scale-x-100 after:transition-transform after:duration-300 group"
                 >
                   Shop Now
-                  <span class="inline-block transition-transform duration-300 group-hover:translate-x-1.5">
+                  <span className="inline-block transition-transform duration-300 group-hover:translate-x-1.5">
                     →
                   </span>
                 </a>
               </button>
             </div>
             <img className="mt-[15px]" src="/benner.png" alt="benner" />
+          </div> */}
+
+          <div
+            id="default-carousel"
+            className="relative w-full"
+            data-carousel="slide"
+          >
+            <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+              <div className=" duration-700 ease-in-out" data-carousel-item>
+                <div className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                  <div className="bg-[#000000] flex gap-[38px] mt-[40px]">
+                    <div className="pt-[58px] ml-[64px]">
+                      <div className="flex items-center gap-[24px] mb-[20px]">
+                        <i className="fa-brands fa-apple text-[40px] text-[#FFF]"></i>
+                        <span className="text-[16px] font-Poppipns_FONT font-medium leading-6 text-[#FFF]">
+                          iPhone 14 Series
+                        </span>
+                      </div>
+                      <h1 className="text-[48px] font-Inter_FONT font-semibold text-[#FAFAFA] leading-[60px] w-[289px] mb-[22px]">
+                        Up to 10% off Voucher
+                      </h1>
+                      <button className=" mb-[48px]">
+                        <a
+                          href="#"
+                          className="relative text-white text-lg font-medium inline-flex items-center gap-2 after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-[81px] hover:after:w-full after:ease-in-out after:h-[1.5px] after:bg-white after:scale-x-100 after:transition-transform after:duration-300 group"
+                        >
+                          Shop Now
+                          <span className="inline-block transition-transform duration-300 group-hover:translate-x-1.5">
+                            →
+                          </span>
+                        </a>
+                      </button>
+                    </div>
+                    <img className="mt-[15px]" src="/benner.png" alt="benner" />
+                  </div>
+                </div>
+              </div>
+              <div className=" duration-700 ease-in-out" data-carousel-item>
+                <div className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                  <div className="bg-[#000000] flex gap-[38px] mt-[40px]">
+                    <div className="pt-[58px] ml-[64px]">
+                      <div className="flex items-center gap-[24px] mb-[20px]">
+                        <i className="fa-brands fa-apple text-[40px] text-[#FFF]"></i>
+                        <span className="text-[16px] font-Poppipns_FONT font-medium leading-6 text-[#FFF]">
+                          iPhone 14 Series
+                        </span>
+                      </div>
+                      <h1 className="text-[48px] font-Inter_FONT font-semibold text-[#FAFAFA] leading-[60px] w-[289px] mb-[22px]">
+                        Up to 10% off Voucher
+                      </h1>
+                      <button className=" mb-[48px]">
+                        <a
+                          href="#"
+                          className="relative text-white text-lg font-medium inline-flex items-center gap-2 after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-[81px] hover:after:w-full after:ease-in-out after:h-[1.5px] after:bg-white after:scale-x-100 after:transition-transform after:duration-300 group"
+                        >
+                          Shop Now
+                          <span className="inline-block transition-transform duration-300 group-hover:translate-x-1.5">
+                            →
+                          </span>
+                        </a>
+                      </button>
+                    </div>
+                    <img className="mt-[15px]" src="/benner.png" alt="benner" />
+                  </div>
+                </div>
+              </div>
+              <div className=" duration-700 ease-in-out" data-carousel-item>
+                <div className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                  <div className="bg-[#000000] flex gap-[38px] mt-[40px]">
+                    <div className="pt-[58px] ml-[64px]">
+                      <div className="flex items-center gap-[24px] mb-[20px]">
+                        <i className="fa-brands fa-apple text-[40px] text-[#FFF]"></i>
+                        <span className="text-[16px] font-Poppipns_FONT font-medium leading-6 text-[#FFF]">
+                          iPhone 14 Series
+                        </span>
+                      </div>
+                      <h1 className="text-[48px] font-Inter_FONT font-semibold text-[#FAFAFA] leading-[60px] w-[289px] mb-[22px]">
+                        Up to 10% off Voucher
+                      </h1>
+                      <button className=" mb-[48px]">
+                        <a
+                          href="#"
+                          className="relative text-white text-lg font-medium inline-flex items-center gap-2 after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-[81px] hover:after:w-full after:ease-in-out after:h-[1.5px] after:bg-white after:scale-x-100 after:transition-transform after:duration-300 group"
+                        >
+                          Shop Now
+                          <span className="inline-block transition-transform duration-300 group-hover:translate-x-1.5">
+                            →
+                          </span>
+                        </a>
+                      </button>
+                    </div>
+                    <img className="mt-[15px]" src="/benner.png" alt="benner" />
+                  </div>
+                </div>
+              </div>
+              <div className=" duration-700 ease-in-out" data-carousel-item>
+                <div className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                  <div className="bg-[#000000] flex gap-[38px] mt-[40px]">
+                    <div className="pt-[58px] ml-[64px]">
+                      <div className="flex items-center gap-[24px] mb-[20px]">
+                        <i className="fa-brands fa-apple text-[40px] text-[#FFF]"></i>
+                        <span className="text-[16px] font-Poppipns_FONT font-medium leading-6 text-[#FFF]">
+                          iPhone 14 Series
+                        </span>
+                      </div>
+                      <h1 className="text-[48px] font-Inter_FONT font-semibold text-[#FAFAFA] leading-[60px] w-[289px] mb-[22px]">
+                        Up to 10% off Voucher
+                      </h1>
+                      <button className=" mb-[48px]">
+                        <a
+                          href="#"
+                          className="relative text-white text-lg font-medium inline-flex items-center gap-2 after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-[81px] hover:after:w-full after:ease-in-out after:h-[1.5px] after:bg-white after:scale-x-100 after:transition-transform after:duration-300 group"
+                        >
+                          Shop Now
+                          <span className="inline-block transition-transform duration-300 group-hover:translate-x-1.5">
+                            →
+                          </span>
+                        </a>
+                      </button>
+                    </div>
+                    <img className="mt-[15px]" src="/benner.png" alt="benner" />
+                  </div>
+                </div>
+              </div>
+              <div className=" duration-700 ease-in-out" data-carousel-item>
+                <div className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+                  <div className="bg-[#000000] flex gap-[38px] mt-[40px]">
+                    <div className="pt-[58px] ml-[64px]">
+                      <div className="flex items-center gap-[24px] mb-[20px]">
+                        <i className="fa-brands fa-apple text-[40px] text-[#FFF]"></i>
+                        <span className="text-[16px] font-Poppipns_FONT font-medium leading-6 text-[#FFF]">
+                          iPhone 14 Series
+                        </span>
+                      </div>
+                      <h1 className="text-[48px] font-Inter_FONT font-semibold text-[#FAFAFA] leading-[60px] w-[289px] mb-[22px]">
+                        Up to 10% off Voucher
+                      </h1>
+                      <button className=" mb-[48px]">
+                        <a
+                          href="#"
+                          className="relative text-white text-lg font-medium inline-flex items-center gap-2 after:content-[''] after:absolute after:left-0 after:-bottom-0.5 after:w-[81px] hover:after:w-full after:ease-in-out after:h-[1.5px] after:bg-white after:scale-x-100 after:transition-transform after:duration-300 group"
+                        >
+                          Shop Now
+                          <span className="inline-block transition-transform duration-300 group-hover:translate-x-1.5">
+                            →
+                          </span>
+                        </a>
+                      </button>
+                    </div>
+                    <img className="mt-[15px]" src="/benner.png" alt="benner" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
+              <button
+                type="button"
+                className="w-3 h-3 rounded-full"
+                aria-current="true"
+                aria-label="Slide 1"
+                data-carousel-slide-to="0"
+              ></button>
+              <button
+                type="button"
+                className="w-3 h-3 rounded-full"
+                aria-current="false"
+                aria-label="Slide 2"
+                data-carousel-slide-to="1"
+              ></button>
+              <button
+                type="button"
+                className="w-3 h-3 rounded-full"
+                aria-current="false"
+                aria-label="Slide 3"
+                data-carousel-slide-to="2"
+              ></button>
+              <button
+                type="button"
+                className="w-3 h-3 rounded-full"
+                aria-current="false"
+                aria-label="Slide 4"
+                data-carousel-slide-to="3"
+              ></button>
+              <button
+                type="button"
+                className="w-3 h-3 rounded-full"
+                aria-current="false"
+                aria-label="Slide 5"
+                data-carousel-slide-to="4"
+              ></button>
+            </div>
+            <button
+              type="button"
+              className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+              data-carousel-prev
+            >
+              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <i class="fa-solid fa-arrow-left"></i>
+              </span>
+            </button>
+            <button
+              type="button"
+              className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+              data-carousel-next
+            >
+              <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                <i class="fa-solid fa-arrow-right"></i>
+              </span>
+            </button>
           </div>
         </Container>
       </section>
--- src/Components/Benner_2/Benner_2.jsx
+++ src/Components/Benner_2/Benner_2.jsx
@@ -1,34 +1,76 @@
 import React from 'react';
 import Container from '../../Container/Container';
+import Benner_3 from '../Benner_3/Benner_3';
 
 const Benner_2 = () => {
   return (
-    <section className="mb-[200px]">
-      <Container>
-        <div className="mb-[40px]">
-          <div className="flex items-center space-x-2 ">
-            <span className="w-[20px] h-[40px] bg-red-500 rounded-md"></span>
-            <span className="text-[#DB4444] text-[18px] font-Poppipns_FONT  font-semibold leading-[20px]">
-              Features
-            </span>
-          </div>
+    <>
+      <section>
+        <Container>
+          <div className="mb-[40px]">
+            <div className="flex items-center space-x-2 ">
+              <span className="w-[20px] h-[40px] bg-red-500 rounded-md"></span>
+              <span className="text-[#DB4444] text-[18px] font-Poppipns_FONT  font-semibold leading-[20px]">
+                Features
+              </span>
+            </div>
 
-          <div className="flex justify-between items-center">
-            <h2 className="text-[36px] font-Inter_FONT font-semibold leading-[48px] text-black mt-[25px]">
-              Features Products
-            </h2>
-            <span className="flex items-center gap-[12px] ">
-              <button class=" bg-gray-100 hover:bg-gray-200 focus:outline-none after:content[''] after:w-[48px] after:h-[48px] rounded-full px-[12px] pt-[10px] pb-[9px]">
-                <i class="fa-solid fa-arrow-left text-[24px]"></i>
-              </button>
-              <button class=" bg-gray-100 hover:bg-gray-200 focus:outline-none after:content[''] after:w-[48px] after:h-[48px] rounded-full px-[12px] pt-[10px] pb-[9px]">
-                <i class="fa-solid fa-arrow-right text-[24px]"></i>
-              </button>
-            </span>
+            <div className="flex justify-between items-center">
+              <h2 className="text-[36px] font-Inter_FONT font-semibold leading-[48px] text-black mt-[25px]">
+                Features Products
+              </h2>
+              <span className="flex items-center gap-[12px] ">
+                <button class=" bg-gray-100 hover:bg-gray-200 focus:outline-none after:content[''] after:w-[48px] after:h-[48px] rounded-full px-[12px] pt-[10px] pb-[9px]">
+                  <i class="fa-solid fa-arrow-left text-[24px]"></i>
+                </button>
+                <button class=" bg-gray-100 hover:bg-gray-200 focus:outline-none after:content[''] after:w-[48px] after:h-[48px] rounded-full px-[12px] pt-[10px] pb-[9px]">
+                  <i class="fa-solid fa-arrow-right text-[24px]"></i>
+                </button>
+              </span>
+            </div>
           </div>
-        </div>
-      </Container>
-    </section>
+          <div className="flex items-center gap-[30px] mb-[80px]">
+            <div className="main_1 w-[170px] h-[145px] flex justify-center items-center flex-col border border-[#000000]/30 rounded-[4px] hover:bg-[#DB4444] ease-in-out duration-300 group cursor-pointer">
+              <i class="fa-thin fa-mobile text-[56px] mb-[18px] group-hover:text-[#FFF] ease-in-out duration-300"></i>
+              <span className="text-[16px] font-Poppipns_FONT font-medium text-[#000] leading-6 group-hover:text-[#FFF] ease-in-out duration-300">
+                Phones
+              </span>
+            </div>
+            <div className="main_2 w-[170px] h-[145px] flex justify-center items-center flex-col border border-[#000000]/30 rounded-[4px] hover:bg-[#DB4444] ease-in-out duration-300 group cursor-pointer">
+              <i class="fa-thin fa-desktop text-[56px] mb-[18px] group-hover:text-[#FFF] ease-in-out duration-300"></i>
+              <span className="text-[16px] font-Poppipns_FONT font-medium text-[#000] leading-6 group-hover:text-[#FFF] ease-in-out duration-300">
+                Computers
+              </span>
+            </div>
+            <div className="main_3 w-[170px] h-[145px] flex justify-center items-center flex-col border border-[#000000]/30 rounded-[4px] hover:bg-[#DB4444] ease-in-out duration-300 group cursor-pointer">
+              <i class="fa-thin fa-watch-smart text-[56px] mb-[18px] group-hover:text-[#FFF] ease-in-out duration-300"></i>
+              <span className="text-[16px] font-Poppipns_FONT font-medium text-[#000] leading-6 group-hover:text-[#FFF] ease-in-out duration-300">
+                SmartWatch
+              </span>
+            </div>
+            <div className="main_4 w-[170px] h-[145px] flex justify-center items-center flex-col border border-[#000000]/30 rounded-[4px] hover:bg-[#DB4444] ease-in-out duration-300 group cursor-pointer">
+              <i class="fa-thin fa-camera text-[56px] mb-[18px] group-hover:text-[#FFF] ease-in-out duration-300"></i>
+              <span className="text-[16px] font-Poppipns_FONT font-medium text-[#000] leading-6 group-hover:text-[#FFF] ease-in-out duration-300">
+                Camera
+              </span>
+            </div>
+            <div className="main_5 w-[170px] h-[145px] flex justify-center items-center flex-col border border-[#000000]/30 rounded-[4px] hover:bg-[#DB4444] ease-in-out duration-300 group cursor-pointer">
+              <i class="fa-thin fa-headphones text-[56px] mb-[18px] group-hover:text-[#FFF] ease-in-out duration-300"></i>
+              <span className="text-[16px] font-Poppipns_FONT font-medium text-[#000] leading-6 group-hover:text-[#FFF] ease-in-out duration-300">
+                HeadPhones
+              </span>
+            </div>
+            <div className="main_6 w-[170px] h-[145px] flex justify-center items-center flex-col border border-[#000000]/30 rounded-[4px] hover:bg-[#DB4444] ease-in-out duration-300 group cursor-pointer">
+              <i class="fa-sharp fa-thin fa-gamepad text-[56px] mb-[18px] group-hover:text-[#FFF] ease-in-out duration-300"></i>
+              <span className="text-[16px] font-Poppipns_FONT font-medium text-[#000] leading-6 group-hover:text-[#FFF] ease-in-out duration-300">
+                Gaming
+              </span>
+            </div>
+          </div>
+        </Container>
+      </section>
+      <Benner_3 />
+    </>
   );
 };
 
