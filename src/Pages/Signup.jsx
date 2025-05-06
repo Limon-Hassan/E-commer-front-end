f094ededf2228b524e4632f1b2dabcbbb850d474
Mahammud Hassan Limon <156072234+Limon-Hassan@users.noreply.github.com>
2025-05-06 18:45:37 +0600

    mamamh

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -31,6 +31,7 @@ const Signup = () => {
         'https://backend-e-commerce-three.vercel.app/api/v1/auth/regisation',
         formData
       );
+      console.log(response);
       localStorage.setItem('authuser', JSON.stringify(response.data.user));
       dispatch(setUser(response.data.user));
 
